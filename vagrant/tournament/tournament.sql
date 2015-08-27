b600df22d73c7dd2f3f4953720747292523fdb76
Nick Iapalucci <cukid9876@outlook.com>
2015-08-26 23:09:51 -0600

    add first CREATE TABLE entries to vagrant/tournament/tournament.sql

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,16 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE TABLE players (
+    player_id serial primary key,
+    name text,
+    wins text,
+    losses text
+);
 
+CREATE TABLE matches (
+    match_id serial primary key,
+    player_1 integer references players(player_id),
+    player_2 integer references players(player_id),
+    winner integer references players(player_id)
+);
